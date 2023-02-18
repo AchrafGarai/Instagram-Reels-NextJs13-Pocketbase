433ae13182dc37ab50e3b255cbfd35658b8d0b41
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2023-02-18 16:02:11 +0100

    Update login.ts

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -35,6 +35,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
+  await runMiddleware(req, res, cors)
   if (req.method === 'POST') {
     try {
       const { email, password } = req.body
